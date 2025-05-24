76a6670df860fd21d5b8f2e04a1219c6d47a5bc3
Abhinav-Eluri <eluriabnav7@gmail.com>
2025-05-24 21:17:52 +0200

    Second version with authors

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -63,7 +63,7 @@ export default function LoginPage() {
                     </button>
                 </form>
                 <p className="text-center text-sm text-gray-500 dark:text-gray-400">
-                    Don't have an account?{' '}
+                    Don&apos;t have an account?{' '}
                     <Link href="/register" className="text-green-600 hover:underline">
                         Register
                     </Link>
